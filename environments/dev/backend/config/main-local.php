f3f6ce2f8718c89b572084dbe9fc35c2b22850c8
Exploit Fate <exploitfate@yahoo.com>
2018-03-05 00:56:14 +0200

    Setup configs

--- environments/dev/backend/config/main-local.php
+++ environments/dev/backend/config/main-local.php
@@ -10,16 +10,23 @@ $config = [
 ];
 
 if (!YII_ENV_TEST) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
+    if (class_exists('yii\debug\Module')) {
+        // configuration adjustments for 'dev' environment
+        $config['bootstrap'][] = 'debug';
+        $config['modules']['debug'] = [
+            'class' => 'yii\debug\Module',
+        ];
+        $config['modules']['debug']['allowedIPs'] = [
+            '127.0.0.1',
+            '::1',
+        ];
+    }
+    if (class_exists('yii\gii\Module')) {
+        $config['bootstrap'][] = 'gii';
+        $config['modules']['gii'] = [
+            'class' => 'yii\gii\Module',
+        ];
+    }
 }
 
 return $config;
