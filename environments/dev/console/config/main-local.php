230888df6eac354440ae05f26848c7d2f83d11c5
Exploit Fate <exploitfate@yahoo.com>
2018-03-04 18:08:08 +0200

    Add stub package

--- environments/dev/console/config/main-local.php
+++ environments/dev/console/config/main-local.php
@@ -4,4 +4,9 @@ return [
     'modules' => [
         'gii' => 'yii\gii\Module',
     ],
+    'controllerMap' => [
+        'stubs' => [
+            'class' => 'bazilio\stubsgenerator\StubsController',
+        ],
+    ],
 ];
