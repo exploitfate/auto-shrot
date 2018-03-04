f4e5c09bf09ecf855ba81f1ddff98c9535e332d2
Exploit Fate <exploitfate@yahoo.com>
2018-03-05 00:40:38 +0200

    Create authManager config

---
+++ common/rbac/Rbac.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace backend\rbac;
+
+/**
+ * Constants for RBAC permission, roles, groups
+ *
+ * @package common\rbac
+ */
+class Rbac
+{
+    const BACKEND_AREA = 'backend-area';
+
+    const BACKEND_MAINTENANCE_RBAC = 'backend-maintenance-rbac';
+}
